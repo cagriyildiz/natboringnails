db65cc6eda3096d75bc942ff2f82f6a509976c5a
cagriyildiz <cagriyild@gmail.com>
2024-04-07 21:52:10 +0200

    Create about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./app.scss"
+import About from "./components/about/About"
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import Treatments from "./components/treatments/Treatments"
@@ -12,8 +13,8 @@ function App() {
         <Hero />
       </section>
       <Treatments />
+      <About />
       <section id="inspiration">Inspiration</section>
-      <section id="about">About</section>
       <section id="contact">Contact</section>
     </>
   )
---
+++ src/components/about/About.tsx
@@ -0,0 +1,27 @@
+import './about.scss'
+
+function About() {
+
+  return (
+    <div id="about" className="about">
+      <div className="title">
+        <h1>About Me</h1>
+      </div>
+      <section>
+        <div className="container">
+          <div className="wrapper">
+            <div className="img">
+              <img src="/nathal5.png" alt="" />
+            </div>
+            <div className="text">
+              <h2>Nathalja Nebbeling</h2>
+              <p>Nails</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default About
