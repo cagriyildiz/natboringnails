a3e0cb96c78ee1d0ac2c94fe0058a5643fd51a11
cagriyildiz <cagriyild@gmail.com>
2024-04-20 10:09:23 +0200

    Adjust the treatments data

--- src/components/treatments/data.ts
+++ src/components/treatments/data.ts
@@ -9,37 +9,43 @@ export const items: Item[] = [
   {
     title: "Gelpolish Manicure",
     price: "€ 65",
-    img: "/nathal.png",
+    img: "/treatments/nails11.jpg",
     desc: "This is our basic treatment. Your nails will get an extended manicure. We take care of your cuticles using an e-file, file your nails in shape and paint your nails in the colour you love. When your nails are done, we nourish your cuticles with some oil and moisturise your hands with hand cream."
   },
   {
     title: "BIAB Manicure",
     price: "€ 75",
-    img: "/nathal2.png",
+    img: "/treatments/nails13.jpg",
     desc: "The BIAB treatment is an excellent choice for those who want natural and strong nails. We take care of your cuticles with an e-file, file your nails in shape and paint your nails one by one with the BIAB colour you love. This treatment will take some time, we'll do your nails one by one. When your nails are done, we nourish your cuticles with some oil and moisturise your hands with hand cream."
   },
   {
     title: "Basic Nail Art",
-    price: "€ 65",
-    img: "/nathal3.png",
+    price: "€ 75",
+    img: "/treatments/nails3.jpg",
     desc: "This is our basic treatment. Your nails will get an extended manicure. We take care of your cuticles using an e-file, file your nails in shape and paint your nails in the colour you love. When your nails are done, we nourish your cuticles with some oil and moisturise your hands with hand cream."
   },
   {
     title: "Advanced Nail Art",
-    price: "€ 65",
-    img: "/nathal4.png",
+    price: "€ 85",
+    img: "/treatments/nails7.jpg",
     desc: "Usually two colours; colour blocking, graphic design, foils, Picasso and simple animal prints. Advanced nail art is including the complete manicure."
   },
   {
     title: "Ultimate Nail Art",
-    price: "€ 65",
-    img: "/nathal5.png",
+    price: "€ 95",
+    img: "/treatments/nails1.jpg",
     desc: "A mix of basic & advanced with more colours and drawings. Think of flames, abstract art, eyes, animal prints. Ultimate nail art is including the complete manicure."
   },
   {
+    title: "Next Level Nail Art",
+    price: "€ 105",
+    img: "/treatments/nails9.jpg",
+    desc: "To keep your nails healthy as possible we require 15 minutes in addition to your appointment to properly remove the gel."
+  },
+  {
     title: "Removal",
-    price: "€ 10",
-    img: "/nathal6.png",
+    price: "€ 15",
+    img: "/treatments/nails14.jpg",
     desc: "To keep your nails healthy as possible we require 15 minutes in addition to your appointment to properly remove the gel."
   },
 ];
