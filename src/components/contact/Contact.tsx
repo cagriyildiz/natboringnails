bb3fcb34afce616cd6ea854fe431b7682137160b
cagriyildiz <cagriyild@gmail.com>
2024-04-21 16:10:42 +0200

    Add basic functionality for sending an email

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,6 +2,13 @@ import './contact.scss'
 
 function Contact() {
 
+  const handleOnSubmit = (e: React.FormEvent) => {
+    const form: HTMLElement = e.target as HTMLElement;
+    const msg: HTMLInputElement | null = form.querySelector("textarea[name='message']");
+    window.location.href = `mailto:nathaljanebbeling@gmail.com?subject=Question - natboringnails.com&body=${msg?.value}`;
+    e.preventDefault();
+  };
+
   return (
     <div id="contact" className="contact">
       <div className="title">
@@ -13,7 +20,7 @@ function Contact() {
             <div className="text">
               <h1>Let's get in touch</h1>
             </div>
-            <form className="form">
+            <form className="form" onSubmit={handleOnSubmit}>
               <div className="item">
                 <label htmlFor="name">Name</label>
                 <input type="text" name="name" placeholder="Your full name" />
