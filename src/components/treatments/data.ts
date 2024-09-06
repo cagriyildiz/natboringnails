10f35161b9219c8b474ec28a6b955d23c785752a
cagriyildiz <cagriyild@gmail.com>
2024-09-06 13:42:53 +0200

    Adjust the pictures of each section in the menu

--- src/components/treatments/data.ts
+++ src/components/treatments/data.ts
@@ -11,42 +11,42 @@ export const items: Item[] = [
     category: "Treatment",
     title: "Gel Polish",
     price: "€ 45",
-    img: "/treatments/gelpolish.jpg",
+    img: "/treatments/gel-polish-treatment.png",
     desc: "In this treatment we give you an extended manicure. This means cuticle care and filing your nails in your desired shape. We then paint them in the colour of your choice! You can also add nail art on top (see nail art menu)."
   },
   {
     category: "Treatment",
     title: "BIAB Natural",
     price: "€ 60",
-    img: "/treatments/biab.jpg",
+    img: "/treatments/biab-treatment.png",
     desc: "The BIAB treatment is an excellent choice for those who want strong natural nails. It builds your nails in such a way that it allows them to grow without breaking! Of course this treatment includes our extended manicure."
   },
   {
     category: "Nail Art",
     title: "Basic",
     price: "Price of treatment + € 10",
-    img: "/treatments/basic.jpg",
+    img: "/treatments/basic-nail-art.png",
     desc: "This nail art can be multiple colours, a dot, some foils, glazed look or some simple line work. For inspiration swipe through the pictures or check out my instagram page."
   },
   {
     category: "Nail Art",
     title: "Medium",
     price: "Price of treatment + € 15",
-    img: "/treatments/advanced.jpg",
+    img: "/treatments/medium-nail-art.png",
     desc: "This nail art can be a french, coloured chrome or a simple airbrush design. For inspiration swipe through the pictures or check out my instagram page."
   },
   {
     category: "Nail Art",
     title: "Extra Extra",
     price: "Price of treatment + € 20",
-    img: "/treatments/ultimate.jpg",
+    img: "/treatments/extra-nail-art.png",
     desc: "With the extra extra nail art we go all out! This nail art can be a combination of different styles like french, chrome or airbrush. But can also include 3D or gems."
   },
   {
     category: "Nail Art",
     title: "Custom",
     price: "Price of treatment + € 30",
-    img: "/treatments/nextlevel.jpg",
+    img: "/treatments/custom-nail-art.png",
     desc: "This option is for those who want something super intricate or personalized. You can always contact me to discuss your ideas!"
   },
   {
