2933f14f2ba003c46adaa9e9841f333e2acc1af9
cagriyildiz <cagriyild@gmail.com>
2025-05-28 18:06:18 +0200

    Add location component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroV2 from "@/components/herov2/HeroV2";
 import ServicesOverview from "@/components/services/ServicesOverview";
 import InspirationPreview from "@/components/inspiration/InspirationPreview";
 import MeetTheTeam from "@/components/about/MeetTheTeam";
+import Location from "@/components/location/Location";
 
 export default function Home() {
   return (
@@ -11,9 +12,9 @@ export default function Home() {
         <ServicesOverview />
         <InspirationPreview />
         <MeetTheTeam />
+        <Location />
         {/*<Treatments />*/}
         {/*<About />*/}
-        {/*<Location />*/}
         {/*<BookingWidget />*/}
       </main>
     </>
