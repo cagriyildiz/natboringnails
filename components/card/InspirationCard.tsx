c5bb088b530b96b81646d56a13b112053ff7c912
cagriyildiz <cagriyild@gmail.com>
2026-01-03 21:18:29 +0100

    Remove text from the card

--- components/card/InspirationCard.tsx
+++ components/card/InspirationCard.tsx
@@ -24,14 +24,7 @@ const InspirationCard: React.FC<InspirationCardProps> = ({ frontImage, title, de
         />
       </div>
 
-      <div className="p-4 flex-grow flex flex-col justify-between">
-        <h3 className="text-xl font-bold text-gray-900 dark:text-white leading-tight mb-3">
-          {title}
-        </h3>
-        <p className="text-gray-700 dark:text-gray-300 text-sm md:text-base leading-relaxed mb-4">
-          {description}
-        </p>
-      </div>
+
     </div>
   );
 };
